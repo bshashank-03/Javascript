494ecbbebaa5e132b1edcbdf420aaa5f25bb9234
bshashank-03 <148200579+bshashank-03@users.noreply.github.com>
2023-11-18 08:03:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+console.log(typeof(score))//Another way
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33=> 33
+// "33abc" => NAN
+let num = 55
+let numberInboolean = Boolean(num)
+console.log(numberInboolean);
+
+// 1 => true, 0=> false
+// "" => false
+// "shashank" => true
