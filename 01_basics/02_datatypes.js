071c283ddcb742f7009620dc92a8443bff1e43da
bshashank-03 <148200579+bshashank-03@users.noreply.github.com>
2023-11-18 07:12:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all js code as newer Version
+
+// alert(3+3) // We are using Nodejs not browser
+
+// console.log(3 // Code Readability should be high should not practise
+    // +3)
+
+let age = 18
+let isLoggedin = false
+
+// number range 2^53
+// string = ""
+// null => Standalone value ex- let state = null
+// undefined => ex- let state;
+// symbol => uniqueness
+
+console.log(typeof undefined)// undefined
+
+console.log(typeof null)// object
+
