97ec677240d071abba76e43b8e749cc4f0711e9e
bshashank-03 <148200579+bshashank-03@users.noreply.github.com>
2023-11-21 16:45:34 +0000

    Scopes in JS

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,59 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);// chota bacha icecream leskta hai bde se
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))// access before initialization
+// but doesn't give any error
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)// Here throws arror { cannot access before initialization} concept called Hoisting
+const addTwo = function(num){
+    return num + 2
+}
