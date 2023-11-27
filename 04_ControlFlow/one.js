ec3e153e2e8190c2910c53557239d03d4312b9aa
bshashank-03 <148200579+bshashank-03@users.noreply.github.com>
2023-11-27 11:24:28 +0000

    controls in js

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,46 @@
+if( 2== "2" ){
+    console.log("Ececuted");
+}
+
+// == -> Doesn't check type of operands 2 == "2" return true
+// === -> strict check ex- 2 === "2" return false
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
